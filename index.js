db460fcb636635a0ad653e18542fce805fddc612
Meghann Stamper <meg.stamp@yahoo.com>
2023-03-29 16:01:51 -0400

    Firestore stuff

--- index.js
+++ index.js
@@ -14,17 +14,51 @@ initializeApp({
 const db = getFirestore();
 
 //add a product to our products collection
-const candy = {
-    name: "Skittles",
-    unitPrice: 3.99,
-    size: "16 oz",
-    color: "green",
-    inventory: 144,
-    productNumber: "SK007",
+const candy2 = {
+    name: "Twix",
+    unitPrice: 2.99,
+    size: "12 oz",
+    color: "gold",
+    inventory: 288,
+    productNumber: 2,
 }
+//
+// // How to add a document to Firestore, returning a promise
+// db.collection('products').add(candy2)
+//   //promise is a temporary special kind of object in JS that can exist in one of three states. 1 in progress, 2. resolved, or 3. rejected.this returns a promise, let's the code continue on while its running. 
+// .then((doc) => { //are returning a doc
+//     console.log("added doc:" + doc.id)
+//      // not returning a document but a "do"
+//     // I can be sure inside.then() that the first process was completed successfully
+// } )
+// .catch(err => console.log(err)) //rejected = a catch
 
-db.collection('products').add(candy)
-.then(doc => console.log("added doc: " + doc.id))
-.catch(err => console.log(err))
+// how to read a document from Firestore:
+db.collection("products").doc("Tlv8X6KnPnTvvL47nHLe").get()
+    .then(doc => {
+        console.log(doc.data())
+    })
+    // .catch(err => console.log(err)) //can also wtrite it as
+    .catch(err => console.log(err))
+    //db.collection("products").doc ("NUMBER").delete()
+
+    //How to update a document in Firestore:
+    db.collection("products").doc("Tlv8X6KnPnTvvL47nHLe").update({
+        inventory: 555, 
+        customerFavorite: true
+    })
+
+
+
+    //how to get a whole collection:
+
+    db.collection("products").get()
+    .then(collection => {
+        const productList = collection.docs.map(doc =>({...doc.data(),id: doc.id}));
+        console.table(productList);
+    })
+    .catch(console.log);
+
+
 
 
